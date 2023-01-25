2db99bce70f0340e6314c3ed2a75719aedd12b45
irshad <109907155+gofyyyyy@users.noreply.github.com>
2023-01-25 15:53:14 +0530

    Add files via upload

---
+++ tut17.cpp
@@ -0,0 +1,43 @@
+// Why Object-Oriented Programming?
+
+// Before we discuss object-oriented programming, we need to learn why we need object-oriented programming?
+
+//  1)   C++ language was designed with the main intention of adding object-oriented programming to C language
+//  2)   As the size of the program increases readability, maintainability, and bug-free nature of the program decrease.
+//  3)   This was the major problem with languages like C which relied(lag karti thi) upon functions or procedure (hence the name procedural programming language)
+//  4)   As a result, the possibility of not addressing the problem adequately was high
+//  5)   Also, data was almost neglected, data security was easily compromised
+//  6)   Using classes solves this problem by modeling program as a real-world scenario
+
+// Difference between Procedure Oriented Programming and Object-Oriented Programming
+// Procedure Oriented Programming
+
+//  1)   Consists of writing a set of instruction for the computer to follow
+//  2)   The main focus is on functions and not on the flow of data
+//  3)   Functions can either use local or global data
+//  4)   Data moves openly from function to function
+
+// Object-Oriented Programming
+
+//  1)   Works on the concept of classes and object
+//  2)   A class is a template to create objects
+//  3)   Treats data as a critical element
+//  4)   Decomposes the problem in objects and builds data and functions around the objects
+
+// Basic Concepts in Object-Oriented Programming
+
+//  1)   Classes - Basic template for creating objects
+//  2)   Objects – Basic run-time entities
+//  3)   Data Abstraction & Encapsulation – Wrapping data and functions into a single unit
+//  4)   Inheritance – Properties of one class can be inherited into others
+//  5)   Polymorphism – Ability to take more than one forms
+//  6)   Dynamic Binding – Code which will execute is not known until the program runs
+//  7)   Message Passing – message (Information) call format
+
+// Benefits of Object-Oriented Programming
+
+//  1)   Better code reusability using objects and inheritance
+//  2)   Principle of data hiding helps build secure systems
+//  3)   Multiple Objects can co-exist without any interference
+//  4)   Software complexity can be easily managed
+
